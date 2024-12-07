5c93093f0c8b6f78c5282fa38a374d53f20c6ba4
enneract9 <vargunin.iv@gmail.com>
2024-12-08 01:43:13 +0500

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by @_@ on 08.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
